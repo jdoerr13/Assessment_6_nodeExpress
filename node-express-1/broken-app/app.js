93568abb5caaba5d0c38ad5ba01972c85a0b1704
Jaime D <134320299+jdoerr13@users.noreply.github.com>
2023-11-30 08:52:56 -0500

    commit assignment

--- node-express-1/broken-app/app.js
+++ node-express-1/broken-app/app.js
@@ -1,18 +1,49 @@
 const express = require('express');
 let axios = require('axios');
-var app = express();
 
-app.post('/', function(req, res, next) {
+const app = express();//changed from var to const: doesn't need reassigned
+app.use(express.json()); //added this middleware to parse JSON bodies. In the updated code, app.use(express.json()) is added to enable the parsing of JSON bodies in incoming requests.
+
+
+app.get('/', (req, res) => {
+  res.send('Hello, this is a GET request!');
+});
+
+app.post('/', async (req, res, next) => {//better to use async with axios.get
   try {
-    let results = req.body.developers.map(async d => {
-      return await axios.get(`https://api.github.com/users/${d}`);
-    });
-    let out = results.map(r => ({ name: r.data.name, bio: r.data.bio }));
-
-    return res.send(JSON.stringify(out));
-  } catch {
+    //change from let to const below
+    const results = await Promise.all( //added this to await for array of promises to resolve.
+      req.body.developers.map(async d => {
+      //add variable for the response and don't return yet!
+         const response = await axios.get(`https://api.github.com/users/${d}`);
+         return { name: response.data.name , bio: response.data.bio };
+    })
+    // let out = results.map(r => ({ name: r.data.name, bio: r.data.bio }));
+    );
+    return res.json(results);//res.json(results) is used, which is a more concise and expressive way to send JSON responses. It automatically sets the appropriate Content-Type header for JSON.
+  } catch (err) {//added err to handle errors properly- needed to pass to next
     next(err);
   }
 });
 
-app.listen(3000);
+app.listen(3000, function () {
+  console.log("Server starting on port 3000")
+}) // added the function bc it was missing
+
+
+
+//OLD WAY
+// app.post('/', function(req, res, next) {
+//   try {
+//     let results = req.body.developers.map(async d => {
+//       return await axios.get(`https://api.github.com/users/${d}`);
+//     });
+//     let out = results.map(r => ({ name: r.data.name, bio: r.data.bio }));
+
+//     return res.send(JSON.stringify(out));
+//   } catch {
+//     next(err);
+//   }
+// });
+
+// app.listen(3000);
--- node-express-1/broken-app/issues.md
+++ node-express-1/broken-app/issues.md
@@ -1,1 +1,37 @@
 # Broken App Issues
+(*see notes in app.js
+)
+1. const app = express();:
+
+Changed from var to const to declare the app variable. Using const is a best practice when the variable doesn't need to be reassigned, promoting immutability and preventing accidental reassignment.
+
+2. app.use(express.json());:
+
+Added middleware to parse JSON bodies in incoming requests. This is essential when working with JSON data in the request body, making it accessible via req.body.
+
+3. async (req, res, next) => { ... }:
+
+Used async with the route handler function to allow the use of await for asynchronous operations, such as the axios.get calls.
+
+4. const results = await Promise.all(...);:
+
+Replaced let with const for results to signal that the variable won't be reassigned.  
+5. Also, added Promise.all to wait for all asynchronous operations to complete before moving forward. This ensures that all requests are sent concurrently, improving performance.
+
+6. const response = await axios.get(...);:
+
+Introduced a const variable response to store the result of the asynchronous operation. This improves readability and prevents accidental reassignment.
+
+7. return res.json(results);:
+
+Used res.json(results) instead of res.send(JSON.stringify(out)) for a more concise and expressive way to send JSON responses. It automatically sets the appropriate Content-Type header for JSON.
+
+8. catch (err) { next(err); }:
+
+Added err to the catch block to handle errors properly. Passing err to next ensures that errors are forwarded to the error-handling middleware.
+
+9. app.listen(3000, () => { ... });:
+
+Added the missing arrow function for better syntax and readability when logging the server start message.
+
+10. added a get request to access the homepage.
